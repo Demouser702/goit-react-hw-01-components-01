44db03fee3f02091fb0457527a36f77d504bb700
roxana.orha <roxanapascaru@gmail.com>
2023-11-09 12:44:47 +0200

    final revision 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,48 +1,48 @@
-// import './App.css';
-import Profile from './profile/Profile';
-import user from './profile/user.json';
-// import data from './data.json';
-// import Statistics from './components/Statistics/Statistics';
-// import FriendList from './components/FriendList/FriendList';
-// import friends from './friends.json';
-// import transactions from './transactions.json';
-// import TransactionHystory from './components/TransactionHistory/TransactionHistory';
+// // import './App.css';
+// import Profile from './profile/Profile';
+// import user from './profile/user.json';
+// // import data from './data.json';
+// // import Statistics from './components/Statistics/Statistics';
+// // import FriendList from './components/FriendList/FriendList';
+// // import friends from './friends.json';
+// // import transactions from './transactions.json';
+// // import TransactionHystory from './components/TransactionHistory/TransactionHistory';
 
-// function transformDataArrayToObject(dataArray) {
-//   const dataObject = {};
-//   dataArray.forEach(item => {
-// //     dataObject[item.label] = item.percentage;
-// //   });
-// //   return dataObject;
+// // function transformDataArrayToObject(dataArray) {
+// //   const dataObject = {};
+// //   dataArray.forEach(item => {
+// // //     dataObject[item.label] = item.percentage;
+// // //   });
+// // //   return dataObject;
+// // }
+
+// function App() {
+//   return (
+//     <div>
+//       <div>
+//         <h1>Profil</h1>
+//         <Profile
+//           username={user.username}
+//           tag={user.tag}
+//           location={user.location}
+//           avatar={user.avatar}
+//           stats={user.stats}
+//         />
+//       </div>
+//       {/* <div>
+//         <Statistics
+//           title="Statistics Title"
+//           stats={transformDataArrayToObject(data)}
+//         />
+//       </div>
+//       <div>
+//         <FriendList friends={friends}></FriendList>
+//       </div>
+//       <div>
+//         <TransactionHystory items={transactions} />;
+//       </div> */}
+//     </div>
+//   );
 // }
 
-function App() {
-  return (
-    <div>
-      <div>
-        <h1>Profil</h1>
-        <Profile
-          username={user.username}
-          tag={user.tag}
-          location={user.location}
-          avatar={user.avatar}
-          stats={user.stats}
-        />
-      </div>
-      {/* <div>
-        <Statistics
-          title="Statistics Title"
-          stats={transformDataArrayToObject(data)}
-        />
-      </div>
-      <div>
-        <FriendList friends={friends}></FriendList>
-      </div>
-      <div>
-        <TransactionHystory items={transactions} />;
-      </div> */}
-    </div>
-  );
-}
-
-export default App;
+// export default App;
